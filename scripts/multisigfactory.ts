a1a9305c47c893340848d03792545f7ba552e953
Isaac <teryima127@gmail.com>
2023-09-08 01:03:54 +0100

    updated

--- scripts/multisigfactory.ts
+++ scripts/multisigfactory.ts
@@ -4,22 +4,23 @@ async function main() {
   const [admin1, admimn2, admin3, admin4, admin5, spender, sender] = await ethers.getSigners()
   const Owners = [admin1.address, admimn2.address, admin3.address]
 
-  let amount = ethers.parseEther("2")
+  let amount = ethers.parseEther("0.0002")
+
+  // deploy the multisig factory contract
+  const multisigfactory =  await ethers.deployContract("MultiSigFactory", [])
+  // wait for it to deploy
+  await multisigfactory.waitForDeployment()
+
+  console.log(`multisig deployed ${multisigfactory.target}`)
+
+  // call the createMultisigWallet() in the multisig factory
+  // to create a multisig wallet
+  let tnx = await multisigfactory.createMultisigWallet(Owners, {value: ethers.parseEther("0.00003")})
 
   
-  const multiSigfactory = await ethers.deployContract("MultiSigFactory", []);
 
-  await multiSigfactory.waitForDeployment();
 
-  let receipt = await multiSigfactory.createMultisigWallet(Owners, {value: ethers.parseEther("50")})
-  
-  
-  //@ts-ignore
-  let newMultisig = (await receipt.wait())?.logs[0]?.args[0]
-  let  multisigContract = await ethers.getContractAt("IMultisig", newMultisig)
 
-  await multisigContract.createTransaction(amount, sender.address)
-  console.log(await multisigContract.getTransaction(1))
 
 }
 
